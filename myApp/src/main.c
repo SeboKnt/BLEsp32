34292564982328c041bb4f02080fcbbb27a58790
sebastian <sebastian.alxr@gmail.com>
2023-05-18 09:50:00 +0200

    cp example

--- myApp/src/main.c
+++ myApp/src/main.c
@@ -1,12 +1,8 @@
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
+#include <errno.h>
 
 int main(void)
 {
-	while (true) {
-		k_msleep(1000);
-		printk("Hello World again from %s\n", CONFIG_BOARD);
-	}
-	
+
 	return 0;
 }
